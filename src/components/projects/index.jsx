f08655b849f8f47195ecd9869bbdbb840049233c
jumaelmartins <jumaelmartins@gmail.com>
2022-12-26 22:42:52 -0300

    Adicionado Projeto Calculadora IMC

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,8 +1,8 @@
 import Card from '../card'
 import { Container, ProjectContainer, Wrapper, Title, Column, Row} from "./styles";
 import Portifolio from "../../assets/img/portifolio.png"
-import EmBreve from "../../assets/img/embreve.png"  
-import { ButtonContainer } from '../button/styles';
+import EmBreve from "../../assets/img/embreve.png"
+import CalculadoraImc from "../../assets/img/calculadora.imc.png"  
 import Button from '../button';
 
 
@@ -14,8 +14,8 @@ const Projects = () => {
             <ProjectContainer>
                 <Column>
                     <Row>
-                        <Card title={'PROJETO 1'} img={Portifolio} run={'https://portfolio-two-wheat-83.vercel.app/'} repo={'https://github.com/jumaelmartins/portfolio'}/>
-                        <Card title={'PROJETO 2'} img={EmBreve}/>
+                        <Card title={'MEU PORTIFOLIO'} img={Portifolio} run={'https://portfolio-two-wheat-83.vercel.app/'} repo={'https://github.com/jumaelmartins/portfolio'}/>
+                        <Card title={'CALCULADORA IMC'} img={CalculadoraImc} run={'https://jumaelmartins.github.io/Calculadora-IMC/'} repo={'https://github.com/jumaelmartins/Calculadora-IMC.git'}/>
                         <Card title={'PROJETO 3'} img={EmBreve}/>
                     </Row>
                     <Row>
